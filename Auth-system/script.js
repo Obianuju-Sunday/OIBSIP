c161733b626972f5539a1b34dbbe346c72667509
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-03-13 14:17:15 +0100

    started the login logic

--- Auth-system/script.js
+++ Auth-system/script.js
@@ -9,3 +9,42 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function signUp() {
+    // Get user information from form
+
+    var fullName = document.getElementById('fullName').value;
+    var userName = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var passwordConfirm = document.getElementById('passwordConfirm').value;
+
+    // const signUpApi = 'https://invest-iq.onrender.com/api/signup';
+    const signUpApi = 'http://localhost:3000/api/auth/signup';
+
+    var requestConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: userName,
+            fullName: fullName,
+            email: email,
+            password: password,
+            passwordConfirm: passwordConfirm
+        })
+    };
+
+    fetch(signUpApi, requestConfig)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert('Signup successful! User details saved to database.')
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Signup failed. Please try again.');
+        });
+}
+
