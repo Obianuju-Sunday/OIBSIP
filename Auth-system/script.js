cde8cc2eed2877077ccbffc39cf460cf377ad5ce
Obianuju-Sunday <obianujusunday43@gmail.com>
2024-03-14 16:34:10 +0100

    some progress

--- Auth-system/script.js
+++ Auth-system/script.js
@@ -1,6 +1,10 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const signUpForm = document.getElementById('sign-up');
+const signInForm = document.getElementById('sign-in');
+const signUpBtn = document.getElementById('sign-up-btn')
+const signInBtn = document.getElementById('sign-in-btn')
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -11,13 +15,8 @@ loginBtn.addEventListener('click', () => {
 });
 
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const signUpForm = document.getElementById('sign-up');
-    const signInForm = document.getElementById('sign-in');
-
-    signUpForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+    signUpBtn.addEventListener('click', (e) => {
+        e.preventDefault();
         const username = document.getElementById('userName').value;
         const password = document.getElementById('password').value;
         localStorage.setItem('userName', username);
@@ -25,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Signup successful! Please sign in.');
     });
 
-    signInForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+    signInBtn.addEventListener('click', (e) => {
+        e.preventDefault();
         const username = document.getElementById('userName').value;
         const password = document.getElementById('password').value;
         const storedUsername = localStorage.getItem('userName');
@@ -38,4 +37,3 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Invalid username or password');
         }
     });
-});
